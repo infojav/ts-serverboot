3d76592e03bc0e51029396e4d827e106f9c02020
Javier Perea <javier.perea@outlook.com>
2017-09-04 07:59:23 +0100

    Create notes.js

---
+++ notes.js
@@ -0,0 +1,9 @@
+const a = [
+  "Garden leave”,
+  “Payment in lieu”, 
+  “unused holidays”,
+  “severance”,
+  “The official reason for termination in company records”,
+  "What your company will say to reference-checkers in the future",
+  "Compromise agreement"
+]
